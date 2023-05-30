0b4d6fc34477b68f8b9a027e159a60a152ee601f
prosper edafe <mamusedafe19@gmail.com>
2023-05-30 23:48:18 +0100

    desktop nav created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.scss'
+import HeaderNav from './components/navigation/headerNav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <HeaderNav />
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/navigation/headerNav.tsx
@@ -0,0 +1,37 @@
+import './header.scss';
+import logo from '../../assets/creative hub logo.svg';
+import { NavLink, Link } from 'react-router-dom';
+
+const HeaderNav = () => {
+    return (
+        <header>
+            <nav>
+                <div className='links'>
+                    <Link className='logo' to='/'>
+                        <img src={logo} alt="logo" />
+                    </Link>
+                    <ul>
+                        <li>
+                            <NavLink to='/'>Explore</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/'>Recent Jobs</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/'>Bargain</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/'>Find Talent</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/'>Find Work</NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <Link className='sign__in' to='/'>Sign In</Link>
+            </nav>
+        </header>
+    )
+}
+
+export default HeaderNav;
