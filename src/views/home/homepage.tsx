820d639288183d5443df216d66313539d6c791c4
prosper edafe <mamusedafe19@gmail.com>
2023-06-09 21:47:53 +0100

    ..

--- src/views/home/homepage.tsx
+++ src/views/home/homepage.tsx
@@ -9,10 +9,12 @@ const Home: FC = () => {
     return (
         <div id="home">
             <section id="home__hero">
-                <div>
-                    <h1>Not Creative Right Now</h1>
-                    <p>Hire top-notch mixing and mastering engineers, singers, songwriters, producers, studio musicians etc.</p>
-                    <Search placeholder="Try ‘Dancers in Accra’" className='' />
+                <div id='home__hero__overlay'>
+                    <div id='home__hero__overlay__content'>
+                        <h1>Not Creative Right Now</h1>
+                        <p>Hire top-notch mixing and mastering engineers, singers, songwriters, producers, studio musicians etc.</p>
+                        <Search placeholder="Try ‘Dancers in Accra’" className='' />
+                    </div>
                 </div>
             </section>
             <Creative />
