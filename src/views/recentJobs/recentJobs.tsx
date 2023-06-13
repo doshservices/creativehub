08722563d64c15fbf5e507090eb1369779113b63
prosper edafe <mamusedafe19@gmail.com>
2023-06-13 11:09:19 +0100

    completed recent jobs page

--- src/views/recentJobs/recentJobs.tsx
+++ src/views/recentJobs/recentJobs.tsx
@@ -1,4 +1,56 @@
 import './jobs.scss';
+import stars from '../searchResult/assets/star.svg';
+
+const reviews: Array<object> = [
+    {
+        review: "Awesome job as usual. I'll work with Wale in the future again for sure.",
+        name: "Job Completed by: Shatta Wale"
+    },
+    {
+        review: "I am a repeat client of Shai because of his top quality work and his personable demeanor which makes working him so...",
+        name: "Job Completed by: Shai Hulud"
+    },
+    {
+        review: "AThe creative hub is hands down one of the best resources for artists and designers in the area",
+        name: "Job Completed by: Peter Parker"
+    },
+    {
+        review: "I am a repeat client of Shai because of his top quality work and his personable demeanor which makes working him so...",
+        name: "Job Completed by: Shai Hulud"
+    },
+    {
+        review: "I am a repeat client of Shai because of his top quality work and his personable demeanor which makes working him so...",
+        name: "Job Completed by: Shatta Wale"
+    },
+    {
+        review: "Awesome job as usual. I'll work with Wale in the future again for sure.",
+        name: "Job Completed by: Shatta Wale"
+    },
+    {
+        review: "The creative hub is hands down one of the best resources for artists and designers in the area",
+        name: "Job Completed by: Peter Parker"
+    },
+    {
+        review: "Awesome job as usual. I'll work with Wale in the future again for sure.",
+        name: "Job Completed by: Shatta Wale"
+    },
+    {
+        review: "I am a repeat client of Shai because of his top quality work and his personable demeanor which makes working him so...",
+        name: "Job Completed by: Shai Hulud"
+    },
+    {
+        review: "I am a repeat client of Shai because of his top quality work and his personable demeanor which makes working him so...",
+        name: "Job Completed by: Shai Hulud"
+    },
+    {
+        review: "The creative hub is hands down one of the best resources for artists and designers in the area",
+        name: "Job Completed by: Peter Parker"
+    },
+    {
+        review: "Awesome job as usual. I'll work with Wale in the future again for sure.",
+        name: "Job Completed by: Shatta Wale"
+    },
+]
 
 const RecentJobs = () => {
     return (
@@ -9,7 +61,26 @@ const RecentJobs = () => {
                     <p>Hire top-notch mixing and mastering engineers, singers, songwriters, producers, studio musicians etc.</p>
                 </div>
             </section>
-            <section id="recent__jobs__reviews"></section>
+            <section id="recent__jobs__reviews">
+                <h3>RECENT REVIEWS</h3>
+                <div className="stars">
+                    <img src={stars} alt="" />
+                    <img src={stars} alt="" />
+                    <img src={stars} alt="" />
+                    <img src={stars} alt="" />
+                    <img src={stars} alt="" />
+                </div>
+                <div className="reviews">
+                    {reviews.map((review: any, index: number) => {
+                        return (
+                            <div key={index}>
+                                <p>{review.review}</p>
+                                <p>{review.name}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            </section>
         </section>
     )
 }
